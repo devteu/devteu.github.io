213b0726b7bec27ed3da0b6b50ad793e784549f6
Matheus <matheushenriquegteu@gmail.com>
2021-12-11 17:27:20 -0300

    alteração pedidos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+$('#qtde')[0].value = 0
+$('#total')[0].value = `R$ ${0},00`
+
+$('#qtde').on('change', () => {
+    calculaPedido($('#qtde')[0].value)
+})
+
+$('#opbolos').on('change', () => {
+    calculaPedido($('#qtde')[0].value)
+})
+
+function calculaPedido(qtde) {
+    switch ($('#opbolos')[0].value) {
+        case 'chocolate':
+            $('#total')[0].value = `R$ ${50 * qtde},00`
+            break;
+
+        case 'limao':
+            $('#total')[0].value = `R$ ${80 * qtde},00`
+            break;
+
+        case 'morango':
+            $('#total')[0].value = `R$ ${95 * qtde},00`
+            break;
+
+        case 'ninho':
+            $('#total')[0].value = `R$ ${100 * qtde},00`
+            break;
+
+        default:
+            $('#total')[0].value = `R$ ${0},00`
+            break;
+    }
+
+
+}
+
 
